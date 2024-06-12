0deeb1a70c0243e61bd05ec6350c518f6fbb404e
Dev2vinod <vinodsinghfun@gmail.com>
2024-06-12 10:32:12 +0530

    installment of react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Card />
+     hell ji ksee ho
+     </>
+  )
+}
+
+export default App
